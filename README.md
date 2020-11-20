e30e951c613bd52fa8b07dc76aa61b81b2483e16
hexsorcerer <hexsorcer03e9@gmail.com>
2020-11-20 12:47:14 -0500

    Add project files
    This project didn't start out in Git, it was intended to be a throwaway. Now
    that I need to make it public to collaborate with someone on it, just dumping
    it in here for now.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # netmq-monitor-client
 A sample application to figure out how the NetMQMonitor class works
+
+This was a quickly-thrown-together prototype to try and understand the
+NetMQMonitor class. I'm only making it public because I'm collaborating with
+someone to try and figure it out. It may or may not disappear when we're done.
+
+If we are able to figure it out, I may polish it up into a quality sample that
+others can use.
---
+++ src/NetMq.Monitor.Client/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Monitoring;
+using NetMQ.Sockets;
+
+namespace DealerSocketTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dealerSocket = new DealerSocket();
+            var poller = new NetMQPoller();
+            var endpoint = "tcp://192.168.2.128:49152";
+
+            dealerSocket.Options.TcpKeepalive = true;
+            dealerSocket.Options.TcpKeepaliveInterval = TimeSpan.FromMilliseconds(1000);
+
+            poller.Add(dealerSocket);
+            poller.RunAsync();
+
+            var monitor = new NetMQMonitor(dealerSocket, endpoint);
+            monitor.Connected += OnConnected;
+            monitor.Disconnected += OnDisconnected;
+            monitor.Timeout = TimeSpan.FromMilliseconds(100);
+            var monitorTask = Task.Factory.StartNew(monitor.Start);
+
+            NetMQMessage message = new NetMQMessage();
+            while (true)
+            {
+                PrintMenu();
+                Console.WriteLine("Enter your selection: ");
+                var selection = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch(selection)
+                {
+                    case 1:
+                        dealerSocket.Connect(endpoint);
+                        Console.WriteLine($"Dealer socket connected to {endpoint}");
+                        break;
+                    case 2:
+                        dealerSocket.Disconnect(endpoint);
+                        Console.WriteLine($"Dealer socket disconnected from {endpoint}");
+                        break;
+                    case 3:
+                        SendMessage(dealerSocket);
+                        break;
+                    case 4:
+                        message = WaitForMessage(dealerSocket);
+                        var itr = message.GetEnumerator();
+                        Console.WriteLine(
+                            string.Format("{0} greeting frame", itr.MoveNext() ? "found" : "not found"));
+                        Console.WriteLine(
+                            string.Format("Message: {0}", itr.Current.ConvertToString()));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            };
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine();
+            Console.WriteLine("1: Connect");
+            Console.WriteLine("2: Disconnect");
+            Console.WriteLine("3: Send a message");
+            Console.WriteLine("4: Wait for a message");
+            Console.WriteLine();
+        }
+
+        private static void SendMessage(DealerSocket socket)
+        {
+            var message = new NetMQMessage();
+            Console.WriteLine("Type the message you want to send: ");
+            var body = Console.ReadLine();
+            message.Append(body);
+
+            socket.SendMultipartMessage(message);
+            Console.WriteLine();
+            Console.WriteLine("Sent message to router socket");
+            Console.WriteLine();
+        }
+
+        private static NetMQMessage WaitForMessage(DealerSocket socket)
+        {
+            Console.WriteLine("Waiting on message from router socket");
+            return socket.ReceiveMultipartMessage();
+        }
+
+        private static void OnConnected(object sender, NetMQMonitorSocketEventArgs e)
+        {
+            Console.WriteLine("Connected");
+        }
+
+        private static void OnDisconnected(object sender, NetMQMonitorSocketEventArgs e)
+        {
+            Console.WriteLine("Disconnected");
+        }
+    }
+}
