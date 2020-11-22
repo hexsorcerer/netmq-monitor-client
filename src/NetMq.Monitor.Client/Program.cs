544d7408d2f8172d0e271d28d2f5ed7c62268b68
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-11-22 17:07:34 -0500

    Add comments
    Added lots of comments so everything is as clear as can be.

--- src/NetMq.Monitor.Client/Program.cs
+++ src/NetMq.Monitor.Client/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * A sample application showing the usage of NetMQMonitor.
+ *
+ * I created this sample application because figuring out how to use the monitor
+ * was like most things in software development...easy once you know how, but
+ * not at all obvious if you don't.
+ *
+ * This provides me with a space to play around with it, learn it, know it,
+ * master its' finer points...and also for anyone else who might be struggling
+ * to figure this thing out.
+ *
+ * It was written to be easy to read, and not cluttered by a lot of things
+ * that are neccessary but unrelated to the monitor itself (settings, etc.).
+ *
+ * References:
+ * https://github.com/zeromq/netmq/blob/master/src/NetMQ.Tests/NetMQMonitorTests.cs
+ * https://csharp.hotexamples.com/examples/-/NetMQMonitor/-/php-netmqmonitor-class-examples.html
+ */
+
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -12,15 +31,37 @@ namespace DealerSocketTest
     {
         static void Main(string[] args)
         {
+            /*
+             * The configuration isn't strictly neccessary for this to work,
+             * it's only here as a convenience.
+             */
             IConfigurationRoot configuration = GetConfiguration();
 
+            // create the client socket and set the options it needs
             var dealerSocket = new DealerSocket();
             SetTcpKeepalive(dealerSocket, configuration);
             SetTcpKeepaliveInterval(dealerSocket, configuration);
 
+            // the poller is needed to know when a message has been received
             var poller = new NetMQPoller();
             RunPoller(poller, dealerSocket);
 
+            /*
+             * Here's where the (non-intuitive) magic happens. You might think
+             * that the monitor endpoint is the endpoint that the monitored
+             * socket is connected to (as I did originally), but that's not
+             * actually correct.
+             *
+             * This is a new endpoint specifically for the monitor, and it
+             * must be inproc (any other protocol will not work). Even though
+             * we're monitoring a TCP connection, the monitor itself is inproc
+             * and so must be attached to this.
+             *
+             * The #monitor can be seen used in the official project in the
+             * tests for the monitor, found in the references listed above.
+             * This doesn't seem to be required, and in my limited testing it
+             * seems any name can be used as long as the protocol is correct.
+             */
             var monitorEndpoint = "inproc://#monitor";
             var monitor = new NetMQMonitor(
                 dealerSocket,
@@ -29,6 +70,10 @@ namespace DealerSocketTest
             SetMonitorOptions(monitor);
             var monitorTask = Task.Factory.StartNew(monitor.Start);
 
+            /*
+             * The rest of this is just a conveninence to work with so you can
+             * see the events happening in real time.
+             */
             var tcpEndpoint = GetTcpEndpoint(configuration);
             NetMQMessage message = new NetMQMessage();
             while (true)
@@ -69,6 +114,22 @@ namespace DealerSocketTest
             };
         }
 
+        /// <summary>
+        /// Gets the configuration from appsettings.json. Settings can be
+        /// accessed by treating the configuration as a dictionary, and using
+        /// the JSON keys as the dictionary keys.
+        /// </summary>
+        /// <example>
+        /// string setting = configuration["JsonKey"];
+        /// </example>
+        /// <remarks>
+        /// Note that all settings are returned as strings. If you need a
+        /// setting with some other type, it will need to be parsed accordingly.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="IConfigurationRoot"/> containing the contents
+        /// of the appsettings.json file.
+        /// </returns>
         private static IConfigurationRoot GetConfiguration()
         {
             return new ConfigurationBuilder()
@@ -80,6 +141,12 @@ namespace DealerSocketTest
                 .Build();
         }
 
+        /// <summary>
+        /// Reads the value of the TcpKeepalive setting from the appsettings
+        /// file, and then writes it into the related option on the socket.
+        /// </summary>
+        /// <param name="dealerSocket">The socket to set the options for.</param>
+        /// <param name="configuration">The appsettings containing the setting.</param>
         private static void SetTcpKeepalive(
             DealerSocket dealerSocket,
             IConfigurationRoot configuration)
@@ -88,6 +155,13 @@ namespace DealerSocketTest
             dealerSocket.Options.TcpKeepalive = tcpKeepalive;
         }
 
+        /// <summary>
+        /// Reads the value of the TcpKeepaliveInterval setting from the
+        /// appsettings file, and then writes it into the related option on the
+        /// socket.
+        /// </summary>
+        /// <param name="dealerSocket">The socket to set the options for.</param>
+        /// <param name="configuration">The appsettings containing the setting.</param>
         private static void SetTcpKeepaliveInterval(
             DealerSocket dealerSocket,
             IConfigurationRoot configuration)
@@ -98,12 +172,25 @@ namespace DealerSocketTest
                 .FromMilliseconds(tcpKeepaliveInterval);
         }
 
+        /// <summary>
+        /// Runs the poller on the given socket. This is how we know when a
+        /// a message has arrived.
+        /// </summary>
+        /// <param name="poller">The poller to run on the socket.</param>
+        /// <param name="dealerSocket">The socket to be polled.</param>
         private static void RunPoller(NetMQPoller poller, DealerSocket dealerSocket)
         {
             poller.Add(dealerSocket);
             poller.RunAsync();
         }
 
+        /// <summary>
+        /// Reads the IP address and port from the configuration file, and
+        /// assembles them into a TCP endpoint that can be used by a socket.
+        /// </summary>
+        /// <param name="configuration">An <see cref="IConfigurationRoot"/>
+        /// containing the appsettings.</param>
+        /// <returns>An endpoint that can be used by a socket.</returns>
         private static string GetTcpEndpoint(IConfigurationRoot configuration)
         {
             var ipAddress = configuration["IpAddress"];
@@ -111,6 +198,10 @@ namespace DealerSocketTest
             return $"tcp://{ipAddress}:{port}";
         }
 
+        /// <summary>
+        /// Configures the monitor.
+        /// </summary>
+        /// <param name="monitor">The monitor to be configured.</param>
         private static void SetMonitorOptions(NetMQMonitor monitor)
         {
             monitor.Connected += OnConnected;
@@ -118,6 +209,9 @@ namespace DealerSocketTest
             monitor.Timeout = TimeSpan.FromMilliseconds(100);
         }
 
+        /// <summary>
+        /// Displays a menu for the user through the console UI.
+        /// </summary>
         private static void PrintMenu()
         {
             Console.WriteLine("Select an option:");
@@ -130,6 +224,10 @@ namespace DealerSocketTest
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Sends a message on the given socket.
+        /// </summary>
+        /// <param name="socket">The socket to send the message on.</param>
         private static void SendMessage(DealerSocket socket)
         {
             var message = new NetMQMessage();
@@ -143,17 +241,33 @@ namespace DealerSocketTest
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Waits for a message on the given socket. Blocks until a message is
+        /// received.
+        /// </summary>
+        /// <param name="socket">The socket waiting for a message.</param>
+        /// <returns>The message that was received.</returns>
         private static NetMQMessage WaitForMessage(DealerSocket socket)
         {
             Console.WriteLine("Waiting on message from router socket");
             return socket.ReceiveMultipartMessage();
         }
 
+        /// <summary>
+        /// Writes to the standard output when a connected event is monitored.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private static void OnConnected(object sender, NetMQMonitorSocketEventArgs e)
         {
             Console.WriteLine("Monitor saw a Connect event");
         }
 
+        /// <summary>
+        /// Writes to the standard output when a disconnected event is monitored.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         private static void OnDisconnected(object sender, NetMQMonitorSocketEventArgs e)
         {
             Console.WriteLine("Monitor saw a Disconnected event");
